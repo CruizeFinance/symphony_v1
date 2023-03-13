c4372c101b2b605347163f43e64ecea2278ff4f7
LilFatFrank <karan.parsnani@ymail.com>
2023-03-13 15:38:23 +0530

    adding a more complete toFixed function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -75,7 +75,7 @@ export function formatNumberSuffix(amount: number) {
   const abbreviations = ['', 'K', 'M', 'B', 'T']
   const abbreviationIndex = Math.floor(Math.log10(amount) / 3)
   return (
-    toFixed((amount / Math.pow(10, abbreviationIndex * 3)), 1) +
+    toFixed(amount / Math.pow(10, abbreviationIndex * 3), 1) +
     abbreviations[abbreviationIndex]
   )
 }
@@ -84,9 +84,29 @@ export function percentge(partialValue: number, totalValue: number) {
   return (100 * partialValue) / totalValue
 }
 
-export function toFixed(num: number, fixed: number) {
-  var re = new RegExp('^-?\\d+(?:\.\\d{0,' + (fixed || -1) + '})?');
-  return num.toString().match(re)![0];
+export function toFixed(num: number, digits: number) {
+  if (isNaN(num)) {
+    return 'NaN'
+  }
+  if (digits < 0) {
+    throw new RangeError('toFixedNoRound() digits argument must be more than 0')
+  }
+  let str = num.toString()
+  let decimalIndex = str.indexOf('.')
+  if (decimalIndex === -1) {
+    return str + '.' + '0'.repeat(digits)
+  } else if (digits === 0) {
+    return str.substring(0, decimalIndex)
+  } else {
+    let numDigitsAfterDecimal = str.length - decimalIndex - 1
+    if (numDigitsAfterDecimal === digits) {
+      return str
+    } else if (numDigitsAfterDecimal < digits) {
+      return str + '0'.repeat(digits - numDigitsAfterDecimal)
+    } else {
+      return str.substring(0, decimalIndex + digits + 1)
+    }
+  }
 }
 
 export * from './constants'
