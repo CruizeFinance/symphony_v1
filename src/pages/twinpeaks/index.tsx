11c50e295aa936dfa9e01f971ff4256af064c107
Prithviraj8 <prithvimurthy8@gmail.com>
2023-04-11 21:26:59 +0530

    change navigation url for asset vaults twinpeaks

--- src/pages/twinpeaks/index.tsx
+++ src/pages/twinpeaks/index.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { Sprite } from '../../components'
 import FaqCard from './faqcard'
 import './twinpeaks.scss'
@@ -8,15 +8,42 @@ import StrategyCard from './strategycard'
 import YieldInfoCard from './yieldinfocard'
 import { AppContext } from '../../context'
 import { Actions } from '../../enums/actions'
+import { Assets } from '../../enums/assets'
 
 const TwinPeaks = () => {
   const [state, dispatch] = useContext(AppContext)
 
   const navigate = useNavigate()
 
+  const location = useLocation()
+
+  const path = location.pathname
+  let vaultAsset = ''
+  if (path.includes('twinpeaks')) {
+    vaultAsset = path.split('twinpeaks/')[1]
+  }
+
   useEffect(() => {
     window.scrollTo(0, 0)
-    dispatch({ type: Actions.SET_BG_COLOR_VALUE, payload: state.selectedAsset })
+
+    if (vaultAsset) {
+      // Sets up the vault page for the desired asset if directed from an external website.
+      // The 1st dispatch is setting the Background color for the asset in the url path and the 2nd dispatch is seelcting the asset in dropdown.
+      dispatch({
+        type: Actions.SET_BG_COLOR_VALUE,
+        payload: vaultAsset,
+      })
+      dispatch({
+        type: Actions.SET_SELECTED_ASSET,
+        payload: vaultAsset,
+      })
+    } else {
+      // This is setting up the default asset on the vault page if the pathname does not have any asset or twinpeaks in it i.e if vault was for ramses-yield-booster.
+      dispatch({
+        type: Actions.SET_BG_COLOR_VALUE,
+        payload: state.selectedAsset,
+      })
+    }
   }, [])
 
   return (
